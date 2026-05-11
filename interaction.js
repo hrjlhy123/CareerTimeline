a406fdf552b2d1197a1f8360f077fdb36b6ee2f7
Ruojie HAO <92860071+hrjlhy123@users.noreply.github.com>
2026-05-10 17:54:13 -0700

    feat: enhance project list hover interactions

--- interaction.js
+++ interaction.js
@@ -1,6 +1,5 @@
 import { getData } from "./3D_model_3.js";
 import { getCoordinates, getRotations } from "./tools/calculate.js"
-import { vec3 } from "./node_modules/gl-matrix/esm/index.js"
 
 "use strict";
 window.addEventListener("DOMContentLoaded", async () => {
@@ -44,53 +43,6 @@ window.addEventListener("DOMContentLoaded", async () => {
                     rz: (data.modelStates[index].angle.rz - data.modelStates[index].deltaAngle.rz) * 180 / Math.PI,
                 };
 
-                // const scale = 1 + z * 0.001;
-                // const center_world = data.results.center; // [x, y, z]
-                const modelPos = vec3.create();
-                const a = [data.modelStates[index].translation.x, data.modelStates[index].translation.y, data.modelStates[index].translation.z]
-                // const b = [data.modelStates[index].center.x, data.modelStates[index].center.y, data.modelStates[index].center.z]
-                const b = [0, 0, 0]
-                // console.log(`data.modelStates[index].translation:`, data.modelStates[index].translation, `data.modelStates[index].center:`, data.modelStates[index].center)
-                vec3.add(modelPos, a, b); // ✅ 真实世界坐标
-                // console.log("world:", modelPos);
-                // const center_camera = vec3.create();
-                // vec3.transformMat4(center_camera, modelPos, data.matrix_view); // 直接用已有 matrix_view
-                // console.log("camera:", center_camera);
-                // console.log(`data.matrix_view:`, data.matrix_view)
-                // const degX = Math.atan2(center_camera[1], center_camera[2]) * 180 / Math.PI;
-                // const degY = -Math.atan2(center_camera[0], center_camera[2]) * 180 / Math.PI;
-                // const degZ = Math.atan2(modelPos[0], modelPos[2]) * 180 / Math.PI;
-                // console.log(`modelPos_fix:`, (modelPos[1] + 20) * 0.15 * Math.cos(modelPos[2]) * -Math.cos(Math.atan2(modelPos[0], modelPos[2])))
-
-                // // 相机视图下的位置
-                // const pos = vec3.create();
-                // const modelPos = vec3.create();
-                // vec3.transformMat4(pos, modelPos, data.matrix_view); // 得到的是视图空间坐标
-
-                // // 在屏幕空间的朝向向量（从相机朝物体）
-                // // console.log(pos[0], pos[1], pos[2])
-                // const screenDir = [pos[0], pos[1]];
-
-                // // 计算这个方向相对于“垂直向上” [0, 1] 的夹角
-                // const degZ = -Math.atan2(screenDir[0], screenDir[1]) * 180 / Math.PI;
-                // console.log(`degZ:`, degZ)
-
-                // const pos = vec3.create(); // 模型世界坐标 → 相机视角
-                // vec3.transformMat4(pos, modelPos, data.matrix_view);
-                // console.log(`modelPos:`, modelPos[0], modelPos[1], modelPos[2])
-
-                // // 用 x/y 得到绕 Z 轴的视觉偏转角度
-                // const degZ = -Math.atan2(pos[0], pos[1]) * 180 / Math.PI;
-
-                //                 // 控制 CSS
-                //                 element.style.transform = `
-                //   rotateX(...) 
-                //   rotateY(...) 
-                //   rotateZ(${trueAngleZ + degZ}deg) // 补偿视觉歪斜
-                // `;
-                // console.log(`degZ:`, degZ)
-
-                // ✅ 你可以控制 CSS 顺序，如先局部再全局：
                 item.style.transform = `
                 rotateX(${angle.global.rx}deg)
                 rotateY(${angle.global.ry}deg)
@@ -99,21 +51,25 @@ window.addEventListener("DOMContentLoaded", async () => {
                 rotateY(${angle.local.ry}deg)
                 rotateZ(${angle.local.rz}deg)
             `;
-                // console.log(`z > data.results.center[2]:`, z > data.results.center[2])
-                // if (index == 1) {
+
                 if (data.modelStates[index].center.z + data.modelStates[index].translation.z < data.results.center[2] - 0.5) {
                     item.style.visibility = `initial`
                 } else {
                     item.style.visibility = `hidden`
                 }
-                // }
 
-                // }
             })
         }
         requestAnimationFrame(frame)
     }
     frame()
+
+
+    let projectShowcase = document.querySelector('div.projectShowcase')
+    let hotzone = document.querySelector(`div.hotzone`)
+    let playzone = document.querySelector(`div.playzone`)
+    let iframeWrappers = document.querySelectorAll(`div.iframe-wrapper`)
+    let iframeMasks = document.querySelectorAll(`div.iframe-mask`)
 
     let year
     li.forEach((item, index) => {
@@ -133,11 +89,6 @@ window.addEventListener("DOMContentLoaded", async () => {
         })
     });
 
-    let projectShowcase = document.querySelector('div.projectShowcase')
-    let hotzone = document.querySelector(`div.hotzone`)
-    let playzone = document.querySelector(`div.playzone`)
-    let iframeWrappers = document.querySelectorAll(`div.iframe-wrapper`)
-    let iframeMasks = document.querySelectorAll(`div.iframe-mask`)
     const bindIframeEvents = () => {
         projectShowcase = document.querySelector('div.projectShowcase');
         iframeWrappers = document.querySelectorAll(`div.iframe-wrapper`);
@@ -177,10 +128,6 @@ window.addEventListener("DOMContentLoaded", async () => {
                 animating = true;
 
                 projectShowcase.classList.add(`active`);
-                // hotzone.style.pointerEvents = `none`;
-                // setTimeout(() => {
-                //     hotzone.style.pointerEvents = `initial`;
-                // }, 2000);
 
                 for (let i = iframeWrappers.length - 1; i >= 0; i--) {
                     iframeWrappers[i].classList.add(`active`);
@@ -196,12 +143,6 @@ window.addEventListener("DOMContentLoaded", async () => {
     };
 
     bindIframeEvents()
-
-    // let i = 0
-    // setInterval(() => {
-    //     iframeWrappers[6].querySelector(`iframe`).src = `https://www.hrjlhy.com/Beijing%20Human%20Resource%20Service%20Industry%20Association/sign_in(` + (i % 4 + 1) + `).html`
-    //     i++
-    // }, 2500);
 
     const applyStackVars = (playzone, maxN = 10) => {
         // 只取已激活的
@@ -294,6 +235,7 @@ window.addEventListener("DOMContentLoaded", async () => {
 
                 hotzoneList.insertAdjacentHTML('beforeend', `<li><span class="project-label"></span></li>`)
                 hotzoneList.lastElementChild.lastElementChild.textContent = name;
+                hotzoneList.lastElementChild.lastElementChild.dataset.text = name;
 
                 playzone.insertAdjacentHTML('beforeend', `
             <div class="iframe-wrapper">
@@ -313,9 +255,11 @@ window.addEventListener("DOMContentLoaded", async () => {
             applyStackVars(playzone, projects.length);
 
         } else {
+            hotzoneList.innerHTML = '';
             projects.forEach(({ name, URLs }) => {
                 hotzoneList.insertAdjacentHTML('beforeend', `<li><span class="project-label"></span></li>`)
                 hotzoneList.lastElementChild.lastElementChild.textContent = name;
+                hotzoneList.lastElementChild.lastElementChild.dataset.text = name;
             })
         }
         // ✅ 重新绑定事件
@@ -337,7 +281,7 @@ window.addEventListener("DOMContentLoaded", async () => {
         }
     };
 
-    const getProjects = async (year) => {
+    const getProjects = (year) => {
         ws.send(JSON.stringify({
             type: 'projects',
             ...(year ? { year } : {}) // 如果有 year 就发送 year，没有就不发送
@@ -561,8 +505,8 @@ window.addEventListener("DOMContentLoaded", async () => {
         host.prepend(blob);
 
         const config = {
-            stiffness: 0.025, // smaller = slower
-            damping: 0.87,    // larger = more elastic
+            stiffness: 0.075, // smaller = slower
+            damping: 0.7,    // larger = more elastic
         };
 
         const state = {
